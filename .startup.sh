3e71eeba5a1af01b1155e9264b9532ca6576b2d9
ebarrere <ebarrere@crexendo.com>
2025-08-28 09:17:39 -0600

    Install chezmoi during bootstrap
    * Install chezmoi during bootstrap
    
    * Add for linux

--- .startup.sh
+++ .startup.sh
@@ -8,6 +8,7 @@ install_on_linux() {
     else
         sh <(curl -L https://nixos.org/nix/install) --daemon
     fi
+    install_chezmoi
 }
 
 install_on_mac() {
@@ -18,6 +19,7 @@ install_on_mac() {
         echo 'eval "$(/opt/homebrew/bin/brew shellenv)"'
     ) >>$HOME/.bashrc
     eval "$(/opt/homebrew/bin/brew shellenv)"
+    install_chezmoi
 }
 
 install_brew() {
@@ -28,12 +30,22 @@ install_brew() {
     fi
 }
 
+install_chezmoi() {
+    if which chezmoi; then
+        echo 'Chezmoi is already installed'
+    else
+        $CHEZMOI_INSTALL_COMMAND
+    fi
+}
+
 OS="$(uname -s)"
 case "${OS}" in
 Linux*)
+    CHEZMOI_INSTALL_COMMAND="nix-env -iA nixpkgs.chezmoi"
     install_on_linux
     ;;
 Darwin*)
+    CHEZMOI_INSTALL_COMMAND="brew install chezmoi"
     install_on_mac
     ;;
 *)
