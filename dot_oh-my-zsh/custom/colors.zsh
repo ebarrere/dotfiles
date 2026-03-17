34eb173e155528d8324197cf1bcbf35221e28970
Elliott Barrere  <ebarrere@crexendo.com>
2026-03-17 06:55:21 -0600

    Updates

---
+++ dot_oh-my-zsh/custom/colors.zsh
@@ -0,0 +1,18 @@
+# Color codes
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+# bold text
+BOLD='\033[1m'
+
+# underline text
+UNDERLINE='\033[4m'
+
+# reverse text
+REVERSE='\033[7m'
+
+# strikethrough text
+STRIKETHROUGH='\033[9m'
