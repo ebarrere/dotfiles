18186807f807f56254b3d6b8d05554ff3b323deb
Elliott Barrere  <ebarrere@crexendo.com>
2026-04-22 16:12:26 -0600

    Make vscode the default

--- dot_oh-my-zsh/custom/editor.zsh
+++ dot_oh-my-zsh/custom/editor.zsh
@@ -1,7 +1,7 @@
-if which cursor &> /dev/null; then
-  export EDITOR='cursor --wait'
-elif which code &> /dev/null; then
+if which code &> /dev/null; then
   export EDITOR='code --wait'
+elif which cursor &> /dev/null; then
+  export EDITOR='cursor --wait'
 else
   export EDITOR='vim'
 fi
